9165418da7aac5e01afc31487042b49afd7630f2
Hussain Basha Shaik <n130547rguktn@gmail.com>
2024-11-04 16:13:11 +0530

    Demo project commit to github

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class LoginPage {
+
+	public void loginpage() {
+		System.out.println("Login Page method");
+	}
+	
+	public void loginSet() {
+		System.out.println("Login Page set method");
+	}
+	
+}
